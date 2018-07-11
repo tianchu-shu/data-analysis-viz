4898802fc19539ed922bce79c2812aa808d2017a
Tianchu <35506630+tianchu-shu@users.noreply.github.com>
2018-07-11 17:34:44 -0400

    Add files via upload

---
+++ candidate_attrition.R
@@ -0,0 +1,252 @@
+# OAE Data Mining Project
+# Written by Tianchu Shu
+# Last Updated July 2018
+
+#Candidate level data 
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(caret)
+library(data.table)
+library(pROC)
+df <- read_csv("C:/Users/tshu/Downloads/Candidate Attrition _05Jul_1401.CSV")
+
+#### Data Pre-processing #####
+colnames(df)[1] <- "OLD_FY"
+#Combine the new and old FY col
+df$FY[!is.na(df$OLD_FY)] = df$OLD_FY[!is.na(df$OLD_FY)] 
+
+df$State[!is.na(df$"HOR State")] = df$"HOR State"[!is.na(df$"HOR State")]
+
+names(df)
+#Drop the cols: OLD_FY, HOR State, and summary
+df<-df[, -c(1, 11, 29, 30, 35)]
+
+#Changing col names
+colnames(df)[13] <-  "Hispanic_Latino"
+colnames(df)[15] <- "Married_DP"
+colnames(df)[16] <- "Serving_Spouse"
+colnames(df)[18] <- "Have_you_been_arres"
+colnames(df)[22] <- "Major"
+colnames(df)[27] <- "Prev_Sub_PC_App"
+colnames(df)[28] <- "PreviouslyServedAsPC"
+colnames(df)[32] <- "Inv_Acpted_Date"
+
+names(df) <- sub(" ", "_", names(df))
+names(df)
+
+#Extract the data of from FY2016 Q4 to FY2018 Q3
+df1 <- subset(df, FY == "FY2018" & Q != "Quarter 4")
+df2 <- subset(df, FY == "FY2015" & Q == "Quarter 4")
+df3 <- subset(df, FY == "FY2017" |FY == "FY2016")
+df <- rbind(df1, df2, df3)
+
+
+#Diversity logic
+df$Diversity[!is.na(df$Race)] <- df$Race[!is.na(df$Race)]
+df$Diversity[df$'Hispanic_Latino' == "Hispanic or Latino"] <- "Hispanic or Latino"
+df$race_count <- sapply(strsplit(df$Race, ","), length)
+df[df$race_count >= 2, ]$Diversity = "Two or More Races"
+sum(is.na(df$Diversity))
+
+#Replace NA
+df$State <- df$State %>% replace_na("No State")
+df$Diversity <- df$Diversity %>% replace_na("unknown")
+df$Have_you_been_arres <- df$Have_you_been_arres %>% replace_na("unknown")
+df$`Married_DP` <- df$`Married_DP` %>% replace_na("No")
+df$`Serving_Spouse` <- df$`Serving_Spouse` %>% replace_na("No")
+df$`Language_Level` <- df$`Language_Level` %>% replace_na("None")
+df$Med_Sort <- df$Med_Sort %>% replace_na("Nomination Cleared")
+#Using the mode 22 to fill the NA for IRT_Score
+df$IRT_Score <- df$IRT_Score %>% replace_na(22)
+
+#Convert the date col to correct type
+df$DOB <- as.Date(df$DOB, "%m/%d/%Y")
+df$EOD_Date <- as.Date(df$EOD_Date, "%m/%d/%Y")
+df$Inv_Acpted_Date <- as.Date(df$Inv_Acpted_Date, "%m/%d/%Y")
+
+#Calculate the candidates' age at the time of accepted their invitation
+x = as.numeric((df$Inv_Acpted_Date - df$DOB) / 365.25)
+df$Age <- floor(x) 
+df <- df[(df$Age >= 18),]
+rm(df1, df2, df3, x)
+
+#df$Sex <- ifelse(df$Sex =="Male", 1,0)
+
+#Drop the old IRT_Scores
+df<-df[, -c(29)]
+#count the NA in specific column
+sum(is.na(df$EOD_Date))
+
+#Calulate the candidate attrition rate for FY16Q4 - FY18Q3
+sum(is.na(df$EOD_Date))/length(df$EOD_Date)
+#0.3751598 of the candidates who got invited ended up not enter on duty
+
+#Created the dummy dependant variable EOD indicate the candidate EOD or not
+df$EOD <- ifelse(is.na(df$EOD_Date), 0 ,1)
+
+sum(df$EOD)/length(df$EOD)
+#0.6248402 of the candidates EOD
+#Catagorize the degree type
+df <- as.data.frame(df)
+df[df$Degree_Type %like% "Bachelor", ]$Degree_Type = "Bachelor"
+df[df$Degree_Type %like% "Doctor", ]$Degree_Type = "Doctorate"
+df[df$Degree_Type %like% "Master", ]$Degree_Type = "Master"
+df[df$Degree_Type %like% "Licensed", ]$Degree_Type = "Master"
+df[df$Degree_Type %like% "Associate", ]$Degree_Type = "Associate"
+df$Degree_Type[is.na(df$Degree_Type)] = "No Degree"
+
+#Matching the education level
+df[df$Education_Level==2, ]$Education_Level = "High school graduate/GED"
+df[df$Education_Level==3, ]$Education_Level = "Incomplete college study"
+df[df$Education_Level==4, ]$Education_Level = "A.A. degree or equivalent"
+df[df$Education_Level==5, ]$Education_Level = "Third year of college completed"
+df[df$Education_Level==6, ]$Education_Level = "College graduate"
+df[df$Education_Level==7, ]$Education_Level = "Graduate study"
+df[df$Education_Level==8, ]$Education_Level = "Graduate degree"
+df[df$Education_Level==9, ]$Education_Level = "Technical school graduate"
+df[df$Education_Level==99, ]$Education_Level = "Other"
+
+#Combine Med_sort
+df[df$Med_Sort == 'Nomination Clear',]$Med_Sort ="Nomination Cleared"
+df[df$Med_Sort=="Triage"| df$Med_Sort=="Nomination Medical Pending",]$Med_Sort = "Medical Pending"
+df[df$Med_Sort=="No additional validation required, cleared for all countries"| df$Med_Sort=="No additional validation required, support required",]$Med_Sort = "Nomination Cleared"
+df[df$Med_Sort=="Additional validation required, support required"| df$Med_Sort=="null",]$Med_Sort = "Nomination Validation Required"
+
+#Merge "Yes" & "Yes_DP"
+df[df$`Serving_Spouse`=="Yes_DP", ]$`Serving_Spouse` = "Yes"
+
+#df$MI <- ifelse(df$MI =="NO", 0, 1)
+
+#Bucket the IRT_Score
+df$IRT <- cut(df$IRT_Score, c(-1,5,10,15,20,25,30))
+summary(df$IRT)
+df$Agebin <- cut(df$Age, c(15,25,30,35,50, 81))
+summary(df$Agebin)
+
+#only keep the features we are going to use
+useful <- c("FY", "Post", "Sector", "State", "Sex", "Diversity", "Married_DP", "Serving_Spouse", "Med_Sort", "Have_you_been_arres", "Education_Level",  "Age", "Agebin", "IRT", "IRT_Score", "Language_Level", "EOD" )
+df <- df[, (names(df) %in% useful)]
+
+
+######## Machine Learning Testing begins ##########
+library("randomForest")
+library("dummies")
+library(class)
+
+head(mni)
+features <- c("FY", "Post", "Sector", "State", "Sex", "Diversity", "Married_DP", "Serving_Spouse", "Med_Sort", "Have_you_been_arres", "Education_Level",  "Age", "IRT_Score", "Language_Level", "EOD" )
+
+mni <- df[, (names(df) %in% features)]
+mni <-as.data.frame(mni)
+#Select the columns for dummify
+dcol <- c("Post", "Sector", "State", "Sex", "Diversity", "Married_DP", "Serving_Spouse", "Language_Level",  "Med_Sort", "Have_you_been_arres", "Education_Level")
+
+#Create dummy vars
+#df1 <- cbind(mni, dummy(df$Q, sep = "_"))
+mydata <- dummy.data.frame(mni, names=dcol, sep="_",  drop = TRUE)
+
+#Drop the old vars
+mydata <- mydata[, !(names(mydata) %in% dcol)]
+names(mydata)
+
+#Train/Test split, using 2015-2017 to predict 2018
+train <- mydata[mydata$FY != "FY2018",]
+test <- mydata[mydata$FY == "FY2018",]
+
+#Drop the FY column
+train <- train[, -c(1)]
+test <- test[, -c(1)]
+
+#rf_model = randomForest(target~. , data = train, ntree =500, importance = TRUE)
+train_x = train[, names(train) !='EOD']
+train_y = train[, names(train) == 'EOD']
+test_x = test[, names(test) != 'EOD']
+test_y = test[, names(test) == 'EOD']
+
+#### K-Nearest Neighbor #####
+knn_pred <- knn(train = train_x, test = test_x,cl = train_y, k=10, prob=TRUE)
+result_k <- confusionMatrix(factor(knn_pred), factor(test_y), mode = "prec_recall", positive="1")
+result_k
+#Precision : 0.6343          
+#Recall : 0.7399          
+#F1 : 0.6831
+
+# ROC area under the curve
+kp=attr(knn_pred,"prob")
+auc(test_y, kp)
+#Area under the curve: 0.5432
+
+
+
+#### Random Foreset #####
+#rf_model = randomForest(x = train_x, y = train_y , ntree = 100, importance = TRUE)
+#these are all the different things you can call from the model.
+#view results
+#print(rf_model)
+#importance of each predictor
+#varImpPlot(rf_model)
+
+
+#rf_pred = predict(rf_model , test_x)
+
+#rf_result <- confusionMatrix(factor(round(y_pred_rf)), factor(test_y), mode = "prec_recall", positive="1")
+#rf_result
+#F1 <- (2 * precision * recall) / (precision + recall)
+
+# ROC area under the curve
+#auc(test_y, rf_pred)
+#Area under the curve: 0.5096
+
+
+#Train/Test split, using 2016, 2017 to predict 2018
+train <- df[df$FY != "FY2018",]
+test <- df[df$FY == "FY2018",]
+head(test)
+#Drop the FY column
+train <- train[, -c(1)]
+test <- test[, -c(1)]
+
+#### Naive Bayes #####
+#Tried adding post and sector, didnt have much differences
+library(e1071)
+nb <- naiveBayes(as.factor(EOD) ~ State + Sex + Married_DP + Serving_Spouse + Med_Sort + Have_you_been_arres+ Education_Level + Language_Level + Agebin + IRT, data=train, laplace = 0, subset, na.action = na.pass)
+nb_pred <- predict(nb, test, type="class")
+nb_result <-confusionMatrix(factor(nb_pred), factor(test$EOD), mode = "prec_recall", positive="1")
+nb_result
+#Precision : 0.6173          
+#Recall : 0.9624          
+#F1 : 0.7521
+
+# ROC area under the curve
+auc(nb_pred, test$EOD)
+#Area under the curve: 0.6151
+
+
+#### SVM #####
+svmodel <- svm(EOD ~ Sex + Married_DP + Serving_Spouse + Med_Sort + Have_you_been_arres+ Education_Level + Language_Level + Age + IRT_Score, data=train)
+svm_pred <- predict(svmodel, test, type ="C-classification")
+svm_result <-confusionMatrix(factor(svm_pred), factor(test$EOD), mode = "prec_recall", positive="1")
+svm_result
+#Precision : 0.6164          
+#Recall : 0.1755          
+#F1 : 0.2732
+
+# ROC area under the curve
+auc(svm_pred, test$EOD)
+#Area under the curve: 1
+
+#### Logistic Regression #####
+mylogit <- lm(EOD ~ State + Sex + Married_DP + Serving_Spouse + Med_Sort + Have_you_been_arres+ Education_Level + Language_Level + Age + IRT_Score, data=train)
+yl = predict(mylogit, test)
+result_logit <- confusionMatrix(factor(round(yl)), factor(test$EOD), mode = "prec_recall", positive="1")
+result_logi
+#Precision : 0.6460          
+#Recall : 0.3366          
+#F1 : 0.4426 
+
+# ROC area under the curve
+auc(test_y, yl)
+#0.5863
+
+
